8dac8285dcc29940db62659ce0e0ccabba59274c
Jay Taylor <outtatime@gmail.com>
2018-06-20 22:56:14 -0700

    CrawlResult.Err -> CrawlResult.ErrMsg.

---
+++ domain/remote_crawler.go
@@ -0,0 +1,22 @@
+package domain
+
+import (
+	"errors"
+)
+
+func NewCrawlResult(pkg *Package, err error) *CrawlResult {
+	cr := &CrawlResult{
+		Package: pkg,
+	}
+	if err != nil {
+		cr.ErrMsg = err.Error()
+	}
+	return cr
+}
+
+func (cr CrawlResult) Error() error {
+	if cr.ErrMsg == "" {
+		return nil
+	}
+	return errors.New(cr.ErrMsg)
+}
