57abf503f1662f00edeadec0a68afc6dcbda31cf
Jay Taylor <outtatime@gmail.com>
2018-07-04 01:17:20 +0000

    pkg/unique.Strings unit-test.

---
+++ pkg/unique/strings_test.go
@@ -0,0 +1,35 @@
+package unique
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestStrings(t *testing.T) {
+	testCases := []struct {
+		in  []string
+		out []string
+	}{
+		{
+			in:  []string{"a"},
+			out: []string{"a"},
+		},
+		{
+			in:  []string{"b", "a"},
+			out: []string{"a", "b"},
+		},
+		{
+			in:  []string{"b", "a", "b"},
+			out: []string{"a", "b"},
+		},
+		{
+			in:  []string{"c", "c", "c"},
+			out: []string{"c"},
+		},
+	}
+	for i, testCase := range testCases {
+		if expected, actual := testCase.out, Strings(testCase.in); !reflect.DeepEqual(actual, expected) {
+			t.Errorf("[i=%v] Expected result=%+v but actual=%+v", i, expected, actual)
+		}
+	}
+}
