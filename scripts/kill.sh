c0fc077a0f54de38ae727155b647071551361563
Jay Taylor <outtatime@gmail.com>
2018-07-08 22:43:10 +0000

    Andromeda process killer utility script.

---
+++ scripts/kill.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [ -z "${1:-}" ] ; then
+    echo 'ERROR: missing required arguemnt: [andromeda-sub-command]' 1>&2
+    exit 1
+fi
+
+set -x
+
+subCommand="$1"
+
+ps -ef | grep andromeda | grep "${subCommand}" | grep -v grep | awk '{ print $2 }' | xargs -n1 kill -9
+
