620e56600480fb76486a5a0ea22f6ce4d799bb5a
Jay Taylor <outtatime@gmail.com>
2018-07-12 01:27:01 +0000

    More granular logging for linux automatic cert acquisition.

--- pkg/openssl/download_cert_pub_key_linux.go
+++ pkg/openssl/download_cert_pub_key_linux.go
@@ -7,15 +7,19 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // DownloadCertPubKey Linux implementation.
 func DownloadCertPubKey(addr string) (string, error) {
 	hostname := strings.Split(addr, ":")[0]
-	cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf("openssl s_client -showcerts -servername %v -connect %v </dev/null 2>/dev/null | openssl x509 -outform PEM", hostname, addr))
+	inner := fmt.Sprintf("openssl s_client -showcerts -servername %v -connect %v </dev/null 2>/dev/null | openssl x509 -outform PEM", hostname, addr)
+	log.Debugf("openssl command: /bin/bash -c '%v'", inner)
+	cmd := exec.Command("/bin/bash", "-c", inner)
 	data, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("downloading certificate public-key: %s", err)
+		return "", fmt.Errorf("downloading certificate public-key: %s (output=%v)", err, string(data))
 	}
 	filename := filepath.Join(os.TempDir(), fmt.Sprintf("%v.pem", hostname))
 	if err := ioutil.WriteFile(filename, data, os.FileMode(int(0600))); err != nil {
