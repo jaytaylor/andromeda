2941cf441c0b2b8e9a0ea6a9d680ba90137ce808
Jay Taylor <outtatime@gmail.com>
2018-07-21 00:22:05 +0000

    Feed data source tests converted to local.
    Rather than integration tests.

--- crawler/feed/find_packages.go
+++ crawler/feed/find_packages.go
@@ -10,8 +10,72 @@ import (
 )
 
 var (
-	validPkgExpr = regexp.MustCompile(`^[^.]+.[^\/]+(?:\/[a-zA-Z0-9_.-]*)*$`)
-	cleanExpr    = regexp.MustCompile(`[^a-zA-Z0-9:\/_.-]+`)
+	validPkgExpr  = regexp.MustCompile(`^[^.]+.[^\/]+(?:\/[a-zA-Z0-9_.-]*)*$`)
+	cleanExpr     = regexp.MustCompile(`[^a-zA-Z0-9:\/_.-]+`)
+	bannedDomains = []string{
+		"ycombinator.com",
+		"reddit.com",
+		"youtube.com",
+		"youtu.be",
+		"ytimg.com",
+		"imgur.com",
+		"bit.ly",
+		// "nytimes.com",
+		// "wired.com",
+		// "wsj.com",
+		// "bloomberg.com",
+		// "slate.com",
+		// "wikipedia.org",
+		// "stackoverflow.com",
+		// "stackexchange.com",
+		// "medium.com",
+		// "qz.com",
+		// "reuters.com",
+		// "arxiv.org",
+		// "archive.is",
+		// "archive.org",
+		// "theverge.com",
+		// "latimes.com",
+		// "newyorder.com",
+		// "boingboing.net",
+		// "arstechnica.com",
+		// "techcrunch.com",
+		// "washingtonpost.com",
+		// "googleblog.com",
+		// "theguardian.com",
+		// "nature.com",
+		// "groups.google.com",
+		// "fda.gov",
+		// "vice.com",
+		// "yahoo.com",
+		// "wordpress.com",
+		// "economist.com",
+		// "acm.org",
+		// "smithsonianmag.com",
+		// "politico.com",
+		// "scientificamerican.com",
+		// "fortune.com",
+		// "cnn.com",
+		// "npr.org",
+		// "cnbc.com",
+		// "gizmodo.com",
+		// "quora.com",
+		// "engadget.com",
+		// "dice.com",
+		// "theatlantic.com",
+		// "sciencemag.org",
+		// "wikileaks.org",
+		// "theintercept.com",
+		// "forbes.com",
+		// "fastcompany.com",
+		// "sfgate.com",
+		// "rollingstone.com",
+		// "bbc.com",
+		// "plus.google.com",
+		// ".blogspot.com",
+		// "torrentfreak.com",
+		// "mercurynews.com",
+	}
 )
 
 // findPackages identifies all possible package paths in a block of text.
@@ -24,10 +88,20 @@ func findPackages(text string) []string {
 			matches[i] = strings.Split(matches[i], "://")[1]
 		}
 		matches[i] = strings.TrimRight(matches[i], "/")
-		if validPkgExpr.MatchString(matches[i]) {
+		if validPkgExpr.MatchString(matches[i]) && !isBannedDomain(matches[i]) {
 			accepted = append(accepted, matches[i])
 		}
 	}
 	u := unique.Strings(accepted)
 	return u
+}
+
+func isBannedDomain(u string) bool {
+	d := strings.SplitN(u, "/", 2)[0]
+	for _, b := range bannedDomains {
+		if strings.HasSuffix(d, b) {
+			return true
+		}
+	}
+	return false
 }
