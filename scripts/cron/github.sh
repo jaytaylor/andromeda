a30733d41ca952a5f5675b963f2bc25007dcb241
Jay Taylor <outtatime@gmail.com>
2018-08-02 21:48:24 +0000

    Dump to file when cron enqueue fails.

--- scripts/cron/github.sh
+++ scripts/cron/github.sh
@@ -25,5 +25,12 @@ if [ -n "${packages}" ] ; then
 
     #andromeda remote enqueue -a localhost:8001 -v -f $(echo "${packages}")
     andromeda remote enqueue -a 127.0.0.1:8001 -v $(echo "${packages}" | tr $'\n' ' ')
+    rc=$?
+    if [ "${rc}" -ne 0 ] ; then
+        echo "Enqueue failed, storing to disk"
+        cd "$(dirname "$0")/../.."
+        mkdir -p pending-enqueues
+        echo "${packages}" > "pending-enqueues/$(basename "$0").$(date '+%Y%m%d_%H%M%S').txt"
+    fi
 fi
 
